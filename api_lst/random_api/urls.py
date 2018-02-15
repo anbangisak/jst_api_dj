b2abec4ebe80e2b85a540c32e89d143a2b1eb44e
Guna Anban <anbangisak@gmail.com>
2018-02-15 13:33:53 +0530

    format support in urls added

--- api_lst/random_api/urls.py
+++ api_lst/random_api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from random_api import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('random-list/', views.random_list),
     path('randoms/<int:pk>/', views.random_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
